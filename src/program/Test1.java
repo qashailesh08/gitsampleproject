ebf2ae066769ee397611810151340c9c135d7611
qashailesh08 <qa.shailesh08@gmail.com>
2024-12-22 18:52:58 +0530

    This is my first commit...

---
+++ src/program/Test1.java
@@ -0,0 +1,12 @@
+package program;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String word = "yes";
+		if (word == "yes") {
+			System.out.println("Eat Someing");
+		} else {
+			System.out.println("Take Rest");
+		}
+	}
+}
